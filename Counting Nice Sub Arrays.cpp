e0c433af53f1e3d1887af4c03113e8cbf0a392a5
ANSH-PANT <131530705+ANSH-PANT@users.noreply.github.com>
2024-01-08 20:06:16 +0530

    Create Counting Nice Sub Arrays.cpp

---
+++ Counting Nice Sub Arrays.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        for (auto& num : nums)
+            if (num % 2)
+                num = 1;
+            else
+                num = 0;
+        mp[0] = 1;
+        int sum = 0;
+        int count = 0;
+        for (auto num : nums) {
+            sum += num;
+            count += mp[sum - k];
+            mp[sum]++;
+        }
+        return count;
+    }
+};
